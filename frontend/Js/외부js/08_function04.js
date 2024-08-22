3b67fa63c684d65970c72c2cd2ac27d17d643dd4
MGiHong <kw3748@gmail.com>
2024-08-22 13:51:04 +0900

    first commit

---
+++ frontend/Js/외부js/08_function04.js
@@ -0,0 +1,26 @@
+var color = ["green", "aqua", "blue", "black"];
+var i = 0;
+
+function changeColor() {
+    i++;
+    if (i >= color.length) i = 0;
+    var bgColor = document.getElementById("bgCol");
+    bgColor.style.backgroundColor = color[i];
+    console.log('bgColor.style' ,'bgColor.style');
+
+    var pColor = document.getElementsByTagName("p")[0];
+    pColor.style.color = "white";
+
+    var hColor = document.getElementsByTagName("h2")[0];
+    hColor.style.color = "white";
+}
+function bgWhite() {
+    var bgColor = document.getElementById("bgCol");
+    bgColor.style.backgroundColor = "white";
+
+    var pColor = document.getElementsByTagName("p")[0];
+    pColor.style.color = "black";
+
+    var hColor = document.getElementsByTagName("h2")[0];
+    hColor.style.color = "black";
+}
